cde7dc3568a4063800b7931c488830dc67b7f498
Anup Agarwal <108anup@gmail.com>
2019-07-10 14:03:24 +0530

    [spacemacs] Use cscope layer
    Change-Id: I1b6e4bef75acbb59f1bafc034fac10b8ff62896f

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -55,6 +55,7 @@ values."
              python-test-runner 'pytest)
      javascript
      yaml
+     cscope
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
