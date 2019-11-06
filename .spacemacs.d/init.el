592307e69178ada3f394be8db3eba73ad6e5d3c6
Anup Agarwal <anupa@andrew.cmu.edu>
2019-11-06 08:46:02 -0700

    [spacemacs, tmux] keys and term

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -6,6 +6,7 @@
   "Configuration Layers declaration.
 You should not put any user code in this function besides modifying the variable
 values."
+  (setq check-package-signature nil)
   (setq-default
    ;; Base distribution to use. This is a layer contained in the directory
    ;; `+distribution'. For now available distributions are `spacemacs-base'
@@ -370,7 +371,47 @@ you should place your code here."
   (push '(other . "linux-tabs-only") c-default-style)
   (push '(other . "linux-spaces-only") c-default-style)
 
+  ;; Keyboard translations for terminal mode
+  ;; Translate C-h to DEL.
+  (keyboard-translate ?\C-h ?\C-?)
 
+  ;; xterm with the resource ?.VT100.modifyOtherKeys: 1
+  ;; GNU Emacs >=24.4 sets xterm in this mode and define
+  ;; some of the escape sequences but not all of them.
+  (defun character-apply-modifiers (c &rest modifiers)
+    "Apply modifiers to the character C.
+MODIFIERS must be a list of symbols amongst (meta control shift).
+Return an event vector."
+    (if (memq 'control modifiers) (setq c (if (or (and (<= ?@ c) (<= c ?_))
+                                                  (and (<= ?a c) (<= c ?z)))
+                                              (logand c ?\x1f)
+                                            (logior (lsh 1 26) c))))
+    (if (memq 'meta modifiers) (setq c (logior (lsh 1 27) c)))
+    (if (memq 'shift modifiers) (setq c (logior (lsh 1 25) c)))
+    (vector c))
+  (defun my-eval-after-load-xterm ()
+    (when (and (boundp 'xterm-extra-capabilities) (boundp 'xterm-function-map))
+      (let ((c 32))
+        (while (<= c 126)
+          (mapc (lambda (x)
+                  (define-key xterm-function-map (format (car x) c)
+                    (apply 'character-apply-modifiers c (cdr x))))
+                '(;; with ?.VT100.formatOtherKeys: 0
+                  ("\e\[27;3;%d~" meta)
+                  ("\e\[27;5;%d~" control)
+                  ("\e\[27;6;%d~" control shift)
+                  ("\e\[27;7;%d~" control meta)
+                  ("\e\[27;8;%d~" control meta shift)
+                  ;; with ?.VT100.formatOtherKeys: 1
+                  ("\e\[%d;3u" meta)
+                  ("\e\[%d;5u" control)
+                  ("\e\[%d;6u" control shift)
+                  ("\e\[%d;7u" control meta)
+                  ("\e\[%d;8u" control meta shift)))
+          (setq c (1+ c))))))
+  (eval-after-load "xterm" '(my-eval-after-load-xterm))
+
+  ;; Fix git-gutter errors
   (with-eval-after-load 'git-gutter+
     (defun git-gutter+-remote-default-directory (dir file)
       (let* ((vec (tramp-dissect-file-name file))
@@ -384,7 +425,6 @@ you should place your code here."
     (defun git-gutter+-remote-file-path (dir file)
       (let ((file (tramp-file-name-localname (tramp-dissect-file-name file))))
         (replace-regexp-in-string (concat "\\`" dir) "" file))))
-  
   )
 
 (setq custom-file "~/.spacemacs.d/custom.el")
