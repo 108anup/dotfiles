032ca3df21654f89bad77f44d6cdc23beb7cda6a
Anup Agarwal <anupa@andrew.cmu.edu>
2020-04-03 17:33:34 -0400

    [zsh] change default editor [spacemacs] Edit faces for monokai

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -437,5 +437,13 @@ Return an event vector."
         (replace-regexp-in-string (concat "\\`" dir) "" file))))
   )
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:background "#131310"))))
+ '(show-paren-match ((t (:background "#131310" :foreground "#66d9ef" :underline t :weight bold)))))
+
 (setq custom-file "~/.spacemacs.d/custom.el")
 (load custom-file)
--- .zshrc
+++ .zshrc
@@ -88,6 +88,8 @@ source $ZSH/oh-my-zsh.sh
 #   export EDITOR='mvim'
 # fi
 
+EDITOR='emacs'
+
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
