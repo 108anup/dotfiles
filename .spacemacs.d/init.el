733d6b417c08524b019bd07ccd9e899af57c711b
Anup Agarwal <anupa@andrew.cmu.edu>
2020-04-26 15:49:48 -0400

    [spacemacs] Update import statement color

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -355,6 +355,7 @@ you should place your code here."
   (add-hook 'org-shiftright-final-hook 'windmove-right)
 
   ;; Bibtex
+
   (defun my/open-pdf-function (fpath link)
     (message fpath)
     (start-process "FoxitReader" "*foxit-reader*" "~/bin/FoxitReader" fpath))
@@ -520,6 +521,7 @@ Return an event vector."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hi-yellow ((t (:background "black" :foreground "yellow1"))))
  '(hl-line ((t (:background "#131310"))))
  '(show-paren-match ((t (:background "#131310" :foreground "#66d9ef" :underline t :weight bold)))))
 
