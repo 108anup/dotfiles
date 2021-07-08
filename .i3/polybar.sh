1fc428fa3f6cd6c5cc9643a2785abe30459682f8
Anup Agarwal <108anup@gmail.com>
2021-07-08 17:49:24 -0500

    [polybar] multi-monitor setup

--- .i3/polybar.sh
+++ .i3/polybar.sh
@@ -7,4 +7,13 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar example -r >> /tmp/polybar.log 2>&1 &
+# polybar example -r >> /tmp/polybar.log 2>&1 &
+
+# Depending on whether multiple monitors are there, launch polybar accordingly.
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar example -r >> /tmp/polybar.log 2>&1 &
+    done
+else
+    polybar example -r >> /tmp/polybar.log 2>&1 &
+fi
