d1ce432cb50ed02ad4346109225a88d162ed2a86
Anup Agarwal <anupa@andrew.cmu.edu>
2023-11-30 13:29:08 -0500

    Add mac install script

--- brew.sh
+++ brew.sh
@@ -5,12 +5,17 @@
 # brew install gpg
 # brew install libressl
 # brew install wget
-brew install bash
-brew install emacs
+brew install bash emacs git tmux
 brew install --cask iterm2
 brew install --cask rectangle
 brew install --cask yandex-disk
 brew install --cask box-sync
 brew install --cask slack
-brew install git
+brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask miniconda
 
+brew tap homebrew/cask-fonts
+brew install font-source-code-pro
+brew install font-source-sans-3
+brew install font-open-sans
