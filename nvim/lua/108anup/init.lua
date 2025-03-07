edeb6bba99cacb906f04ce8d0f260ea4b4d51aac
Anup Agarwal <anupa@andrew.cmu.edu>
2025-03-06 23:40:14 -0500

    [nvim] update

--- nvim/lua/108anup/init.lua
+++ nvim/lua/108anup/init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 require("108anup.lazy_init")
 require("108anup.set")
--- nvim/lua/108anup/remap.lua
+++ nvim/lua/108anup/remap.lua
@@ -1,1 +1,13 @@
+local wk = require("which-key")
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+local builtin = require('telescope.builtin')
+wk.add({
+  { "<leader>f", group = "Find" },
+  { "<leader>fb", builtin.buffers, desc = "Buffers" },
+  { "<leader>ff", builtin.find_files, desc = "Find files" },
+  { "<leader>fg", builtin.git_files, desc = "Find git files" },
+  { "<leader>fh", builtin.help_tags, desc = "Help tags" },
+  { "<leader>fr", builtin.live_grep, desc = "Live grep" },
+})
