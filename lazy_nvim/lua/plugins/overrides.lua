00de28e46e4ba98146fae588b6526d7463945b8e
Anup Agarwal <anupa@andrew.cmu.edu>
2025-03-07 23:41:26 -0500

    [nvim] disable flash, and autoformat

--- lazy_nvim/lua/plugins/overrides.lua
+++ lazy_nvim/lua/plugins/overrides.lua
@@ -1,9 +1,3 @@
 return {
-  -- disable leap
-  { "ggandor/leap.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
-  {
-    "neovim/nvim-lspconfig",
-    opts = { autoformat = false },
-  },
+  { "folke/flash.nvim", enabled = false },
 }
