5d7e1b25d13b8c06faa30a2c4a55c2f120d9d2c0
Anup Agarwal <anupa@andrew.cmu.edu>
2025-03-11 04:02:29 +0000

    ..

--- lazy_nvim/lua/plugins/colorscheme.lua
+++ lazy_nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,23 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "kanagawa-dragon",
     },
-  }
+  },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   opts = {
+  --     term_colors = true,
+  --     transparent_background = false,
+  --     color_overrides = {
+  --       mocha = {
+  --         base = "#000000",
+  --         mantle = "#000000",
+  --         crust = "#000000",
+  --       },
+  --     },
+  --   },
+  -- },
 }
