2fa46ddd8c5aa0908ba41fb89ac53891d8e068c4
Anup Agarwal <anupa@andrew.cmu.edu>
2025-04-12 23:16:55 -0400

    ..

--- lazy_nvim/lua/plugins/cmp.lua
+++ lazy_nvim/lua/plugins/cmp.lua
@@ -11,12 +11,12 @@ return {
 
       local cmp = require("cmp")
 
-      -- -- This ensures that the completion menu is not selected by default
-      -- opts.completion.completeopt = "noselect"
+      -- This ensures that the completion menu is not selected by default
+      opts.completion.completeopt = "noselect"
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        -- -- this forces using Tab to first select the item and then Enter to confirm
-        -- ['<CR>'] = cmp.mapping.confirm { select = false },
+        -- this forces using Tab to first select the item and then Enter to confirm
+        ['<CR>'] = cmp.mapping.confirm { select = false },
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
